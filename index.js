b306e27552843eb0ef152b49e330c88f88e0db41
Ignacio Cunial <igsecu@hotmail.com>
2023-09-12 22:23:09 -0300

    Updated index.js -> modified PORT variable

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
+// Get welcomed route
 app.get("/api", (req, res) => {
   res.json({
     msg: "Welcome to the API",
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server listening on port 5000...");
+// Initialized Server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
 });
